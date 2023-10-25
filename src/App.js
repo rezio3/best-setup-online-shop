93a7c25037c4ec7cbeb54d71975ad66332c63f66
Joszko <jakub.rezler96@gmail.com>
2023-10-25 03:49:35 +0200

    create Navbar.jsx component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Header from "./layouts/Header";
+import Navbar from "./layouts/Navbar";
 
 function App() {
 	return (
 		<div className="App">
 			<Header />
+			<Navbar />
 		</div>
 	);
 }
