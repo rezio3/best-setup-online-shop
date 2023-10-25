00e8d4542cc433eb5c4cc37abf5e546ac236291b
Joszko <jakub.rezler96@gmail.com>
2023-10-25 03:58:19 +0200

    add header components to Header.jsx

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import HeaderLogoBar from "../components/HeaderLogoBar";
+import HeaderCarousel from "../components/HeaderCarousel";
 
 const Header = () => {
 	return (
-		<>
-			<div></div>
-		</>
+		<div className="header-container">
+			<HeaderLogoBar />
+			<HeaderCarousel />
+		</div>
 	);
 };
 
