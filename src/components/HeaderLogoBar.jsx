af90b74d1d4f80a030c4dd889a3550fd166b210a
Joszko <jakub.rezler96@gmail.com>
2023-10-25 04:41:09 +0200

    import logo image to HeaderLogoBar.jsx component

--- src/components/HeaderLogoBar.jsx
+++ src/components/HeaderLogoBar.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import bestSetupLogo from "../img/BestSetupLogo.png";
 
 const HeaderLogoBar = () => {
 	return (
 		<div className="header-logo-bar">
-			<img src="" className="header-logo-bar__logo" />
+			<img src={bestSetupLogo} className="header-logo-bar__logo" />
 			<span className="header-logo-bar__contact-span">Contact Us!</span>
 		</div>
 	);
