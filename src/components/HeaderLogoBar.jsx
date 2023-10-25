889117bb8b76752ca21068bc5cde8a9a9d7ccbe2
Joszko <jakub.rezler96@gmail.com>
2023-10-25 03:57:53 +0200

    create HeaderLogoBar.jsx component

---
+++ src/components/HeaderLogoBar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const HeaderLogoBar = () => {
+	return (
+		<div className="header-logo-bar">
+			<img src="" className="header-logo-bar__logo" />
+			<span className="header-logo-bar__contact-span">Contact Us!</span>
+		</div>
+	);
+};
+
+export default HeaderLogoBar;
