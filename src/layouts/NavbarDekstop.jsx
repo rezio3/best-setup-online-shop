b8846371f930c19531bd2b5646c006c33c4ff18e
Joszko <jakub.rezler96@gmail.com>
2023-10-26 03:37:14 +0200

    update NavbarDesktop.jsx component

--- src/layouts/NavbarDekstop.jsx
+++ src/layouts/NavbarDekstop.jsx
@@ -2,7 +2,38 @@ import React from "react";
 import "../style/css/navbarDesktop.css";
 
 const NavbarDesktop = () => {
-	return <div className="navbar-container"></div>;
+	return (
+		<div className="navbar-container">
+			<ul className="product-pages-buttons-container">
+				<li className="product-pages-buttons-container__nav-element">
+					<button className="product-pages-buttons-container__nav-button">
+						Display
+					</button>
+				</li>
+				<li className="product-pages-buttons-container__nav-element">
+					<button className="product-pages-buttons-container__nav-button">
+						Sound
+					</button>
+				</li>
+				<li className="product-pages-buttons-container__nav-element">
+					<button className="product-pages-buttons-container__nav-button">
+						PC
+					</button>
+				</li>
+				<li className="product-pages-buttons-container__nav-element">
+					<button className="product-pages-buttons-container__nav-button">
+						Accessories
+					</button>
+				</li>
+				<li className="product-pages-buttons-container__nav-element">
+					<button className="product-pages-buttons-container__nav-button">
+						Comfort
+					</button>
+				</li>
+			</ul>
+			<div className="cart-container"></div>
+		</div>
+	);
 };
 
 export default NavbarDesktop;
