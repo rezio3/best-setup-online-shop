1b5b7e82e5a5c2fd73678cdd2463ecbb13d53fbb
Joszko <jakub.rezler96@gmail.com>
2023-10-26 05:17:23 +0200

    add contact button animation

--- src/components/HeaderLogoBar.jsx
+++ src/components/HeaderLogoBar.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import bestSetupLogo from "../img/BestSetupLogo.png";
+import "../style/css/buttons-anim.css";
 
 const HeaderLogoBar = () => {
 	return (
 		<div className="header-logo-bar">
 			<img src={bestSetupLogo} className="header-logo-bar__logo" />
-			<button className="header-logo-bar__contact-button">Contact Us!</button>
+			<button className="header-logo-bar__contact-button button-anim">
+				Contact Us!
+			</button>
 		</div>
 	);
 };
