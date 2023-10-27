0b59964b8649ba788a8733bcfa2d9606b7a47289
Joszko <jakub.rezler96@gmail.com>
2023-10-27 03:32:36 +0200

    add Page component to App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Header from "./layouts/Header";
 import Navbar from "./layouts/Navbar";
+import Page from "./layouts/Page";
 
 function App() {
 	return (
 		<div className="App">
 			<Header />
 			<Navbar />
+			<Page />
 		</div>
 	);
 }
