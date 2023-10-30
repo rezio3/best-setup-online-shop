1830ec9ab0158964ae6297f112693eb92fb6f737
Joszko <jakub.rezler96@gmail.com>
2023-10-30 01:31:54 +0100

    update animation in HeaderCarousel.jsx

--- src/components/HeaderCarousel.jsx
+++ src/components/HeaderCarousel.jsx
@@ -1,26 +1,80 @@
 import React, { useState } from "react";
 
+import Banner from "./Banner";
+
 const HeaderCarousel = () => {
-	let [activeBanner, setActiveBanner] = useState({ active: 1 });
-	const arrowLeft = () => {
-		setActiveBanner({ ...activeBanner, active: activeBanner.active + 1 });
+	let [activeBanner, setActiveBanner] = useState({
+		active: 1,
+	});
+	let [arrowsOff, setArrowsOff] = useState(false);
+	let [whichArrow, setWhichArrow] = useState();
+
+	const arrow = (e) => {
+		let reduction;
+		const arrowClicked = e.target.getAttribute("data");
+		setWhichArrow(arrowClicked);
+		arrowClicked === "left" ? (reduction = -1) : (reduction = +1);
+
+		let resetCarousel;
+		let ifCarouselEnds;
+
+		arrowClicked === "left" ? (resetCarousel = 3) : (resetCarousel = 1);
+		arrowClicked === "left" ? (ifCarouselEnds = 1) : (ifCarouselEnds = 3);
+
+		console.log("ifCarouselEnds: " + ifCarouselEnds);
+		console.log("resetCarousel: " + resetCarousel);
+		console.log("reduction: " + reduction);
+		console.log("arrow clicked: " + arrowClicked);
+
+		if (activeBanner.active === ifCarouselEnds && !arrowsOff) {
+			setActiveBanner({ ...activeBanner, active: resetCarousel });
+			setArrowsOff(true);
+			setTimeout(() => {
+				setArrowsOff(false);
+			}, 400);
+		} else if (!arrowsOff) {
+			setActiveBanner({
+				...activeBanner,
+				active: activeBanner.active + reduction,
+			});
+			setArrowsOff(true);
+			setTimeout(() => {
+				setArrowsOff(false);
+			}, 400);
+		}
 	};
-	console.log(activeBanner.active);
+
 	return (
 		<div className="carousel-container">
-			<div className="arrow-container arrow-container-left" onClick={arrowLeft}>
-				<span className="material-symbols-outlined arrow-icon arrow-icon-left">
+			<div
+				className="arrow-container arrow-container-left"
+				data="left"
+				onClick={arrow}
+			>
+				<span
+					className="material-symbols-outlined arrow-icon arrow-icon-left"
+					data="left"
+				>
 					chevron_left
 				</span>
 			</div>
-			<div className="arrow-container arrow-container-right">
-				<span className="material-symbols-outlined arrow-icon arrow-icon-right">
+			<div
+				className="arrow-container arrow-container-right"
+				data="right"
+				onClick={arrow}
+			>
+				<span
+					className="material-symbols-outlined arrow-icon arrow-icon-right"
+					data="right"
+				>
 					chevron_right
 				</span>
 			</div>
-			<div className="baner-bg1 baner-bg"></div>
-			<div className="baner-bg2 baner-bg"></div>
-			<div className="baner-bg3 baner-bg"></div>
+			<Banner
+				class={`baner-bg${activeBanner.active} baner-bg`}
+				state={activeBanner.active}
+				clicked={whichArrow}
+			/>
 		</div>
 	);
 };
