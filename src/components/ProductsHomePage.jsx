73baa869b776cf2888829a22395598283aa72fc1
Joszko <jakub.rezler96@gmail.com>
2023-10-31 02:29:37 +0100

    generate ProductBox component depends on products object

--- src/components/ProductsHomePage.jsx
+++ src/components/ProductsHomePage.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import "../style/css/productsHomePage.css";
+import ProductBox from "./ProductBox";
+import products from "../objects/products";
 
 const ProductsHomePage = () => {
 	return (
 		<>
 			<div className="products-container">
-				<div></div>
+				{products.hotDeals.map((e) => {
+					return <ProductBox product={e} />;
+				})}
 			</div>
 		</>
 	);
---
+++ src/objects/products.js
@@ -0,0 +1,22 @@
+const products = {
+	hotDeals: [
+		{ name: "Dell-SE2422H", img: 1 },
+		{ name: "G4M3R-HERO", img: 2 },
+		{ name: "Genesis-Xenon-220-G2-RGB", img: 3 },
+		{ name: "Gigabyte-M32QC-Curved-HDR-KVM", img: 4 },
+		{ name: "LG-Ultrawide-34WP65CP-B", img: 5 },
+		{ name: "Razer-Ornata-V2", img: 6 },
+		{ name: "Samsung-Odyssey-C34G55TWWPX", img: 7 },
+		{ name: "Logitech-G502-HERO", img: 8 },
+		{ name: "Silver-Monkey-X-Mandrill", img: 9 },
+		{ name: "Genesis-Xenon-220-G2-RGB", img: 10 },
+		{ name: "Logitech-G502-HERO", img: 8 },
+		{ name: "Silver-Monkey-X-Mandrill", img: 9 },
+		{ name: "Logitech-G502-HERO", img: 8 },
+		{ name: "Genesis-Xenon-220-G2-RGB", img: 10 },
+		{ name: "Gigabyte-M32QC-Curved-HDR-KVM", img: 4 },
+		{ name: "Silver-Monkey-X-Mandrill", img: 9 },
+	],
+};
+
+export default products;
