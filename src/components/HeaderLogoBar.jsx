250ed36a1d53e97763121ec901b76af96beb2f80
Joszko <jakub.rezler96@gmail.com>
2023-10-31 01:40:55 +0100

    update buttonsAnim.css import in header component

--- src/components/HeaderLogoBar.jsx
+++ src/components/HeaderLogoBar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import bestSetupLogo from "../img/BestSetupLogo.png";
-import "../style/css/buttons-anim.css";
+import "../style/css/buttonsAnim.css";
 
 const HeaderLogoBar = () => {
 	return (
