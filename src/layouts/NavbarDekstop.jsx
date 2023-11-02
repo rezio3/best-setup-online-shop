82d1e059a73e5a084308ac120ace45cf1f3d5ebb
Joszko <jakub.rezler96@gmail.com>
2023-11-02 02:55:15 +0100

    add navbar animation according to scroll

--- src/layouts/NavbarDekstop.jsx
+++ src/layouts/NavbarDekstop.jsx
@@ -1,44 +1,63 @@
-import React from "react";
+import React, { useState, useRef, useEffect } from "react";
 import "../style/css/navbarDesktop.css";
 
 const NavbarDesktop = () => {
+	const [snapNav, setSnapNav] = useState(false);
+
+	const navRef = useRef();
+	useEffect(() => {
+		const observer = new IntersectionObserver(
+			(entries) => {
+				const entry = entries[0];
+				entry.intersectionRatio === 0 ? setSnapNav(true) : setSnapNav(false);
+			},
+			{ threshold: 1 }
+		);
+		observer.observe(navRef.current);
+	}, []);
+
 	return (
-		<nav className="navbar-container">
-			<ul className="product-pages-buttons-container">
-				<li className="product-pages-buttons-container__nav-element">
-					<button className="product-pages-buttons-container__nav-button">
-						Display
+		<>
+			<div className="nav-treshhold" ref={navRef}></div>
+			<nav className={snapNav ? "navbar-container sticky" : "navbar-container"}>
+				<div className="navbar-container__empty-left-spacer"></div>
+				<ul className="product-pages-buttons-container">
+					<li className="product-pages-buttons-container__nav-element">
+						<button className="product-pages-buttons-container__nav-button">
+							Display
+						</button>
+					</li>
+					<li className="product-pages-buttons-container__nav-element">
+						<button className="product-pages-buttons-container__nav-button">
+							Sound
+						</button>
+					</li>
+					<li className="product-pages-buttons-container__nav-element">
+						<button className="product-pages-buttons-container__nav-button">
+							PC
+						</button>
+					</li>
+					<li className="product-pages-buttons-container__nav-element">
+						<button className="product-pages-buttons-container__nav-button">
+							Accessories
+						</button>
+					</li>
+					<li className="product-pages-buttons-container__nav-element">
+						<button className="product-pages-buttons-container__nav-button">
+							Comfort
+						</button>
+					</li>
+				</ul>
+				<div className="cart-container">
+					<button className="cart-container__cart-button">
+						<span className="material-symbols-outlined cart-container__cart-icon">
+							shopping_cart
+						</span>
 					</button>
-				</li>
-				<li className="product-pages-buttons-container__nav-element">
-					<button className="product-pages-buttons-container__nav-button">
-						Sound
-					</button>
-				</li>
-				<li className="product-pages-buttons-container__nav-element">
-					<button className="product-pages-buttons-container__nav-button">
-						PC
-					</button>
-				</li>
-				<li className="product-pages-buttons-container__nav-element">
-					<button className="product-pages-buttons-container__nav-button">
-						Accessories
-					</button>
-				</li>
-				<li className="product-pages-buttons-container__nav-element">
-					<button className="product-pages-buttons-container__nav-button">
-						Comfort
-					</button>
-				</li>
-			</ul>
-			<div className="cart-container">
-				<button className="cart-container__cart-button">
-					<span className="material-symbols-outlined cart-container__cart-icon">
-						shopping_cart
-					</span>
-				</button>
-			</div>
-		</nav>
+				</div>
+			</nav>
+			<div className={snapNav ? "spacer" : ""}></div>
+		</>
 	);
 };
 
