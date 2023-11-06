bab0fea7068ac46ce08d4024b4f739867eee2303
Joszko <jakub.rezler96@gmail.com>
2023-11-07 00:27:16 +0100

    create filter mechanism in ProductsHomePage comp

--- src/components/ProductsHomePage.jsx
+++ src/components/ProductsHomePage.jsx
@@ -1,13 +1,26 @@
-import React from "react";
+import React, { useContext } from "react";
 import "../style/css/productsHomePage.css";
 import ProductBox from "./ProductBox";
 import products from "../objects/products";
+import { FilterContext } from "../context/FilterContext";
 
 const ProductsHomePage = () => {
+	const [filter, setFilter] = useContext(FilterContext);
+	const { displays, sounds, pc, accessories, comfort } = filter;
+	let itemsToDisplay = [];
+
+	for (let key in products.hotDeals) {
+		if (products.hotDeals[key].type === "displays" && displays) {
+			itemsToDisplay.push(products.hotDeals[key]);
+			console.log(products.hotDeals[key]);
+		} else if (!displays && !sounds && !pc && !accessories && !comfort) {
+			itemsToDisplay = products.hotDeals;
+		}
+	}
 	return (
 		<>
 			<div className="products-container">
-				{products.hotDeals.map((e) => {
+				{itemsToDisplay.map((e) => {
 					return <ProductBox product={e} key={e.productId} />;
 				})}
 			</div>
