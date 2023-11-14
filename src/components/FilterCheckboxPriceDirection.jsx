db230a1cf4e55d45497d0f731b3c74c027f5da13
Joszko <jakub.rezler96@gmail.com>
2023-11-14 01:31:53 +0100

    rebuild radio checkboxes component

--- src/components/FilterCheckboxPriceDirection.jsx
+++ src/components/FilterCheckboxPriceDirection.jsx
@@ -1,22 +1,38 @@
 import React from "react";
 
 const FilterCheckboxPriceDirection = (props) => {
-	const inputHandler = () => {
+	const inputHandler = (el) => {
+		const directionSetter = el.target.id === "price-up" ? true : false;
 		props.filterState.setFilter({
-			...props.filterState.Filter,
+			...props.filterState.filter,
+			priceUp: directionSetter,
+			priceDown: !directionSetter,
 		});
 	};
+
 	return (
 		<>
 			<li className="filter-container__filter-checkbox-element">
 				<input
 					type="radio"
 					className="filter-container__filter-checkbox"
-					id={props.name}
+					id="price-up"
 					name="price-radio-checkbox"
 					onChange={inputHandler}
+					checked={props.filterState.filter.priceUp}
 				></input>
-				<label htmlFor={props.name}>{props.name}</label>
+				<label htmlFor="price-up">Price up</label>
+			</li>
+			<li className="filter-container__filter-checkbox-element">
+				<input
+					type="radio"
+					className="filter-container__filter-checkbox"
+					id="price-down"
+					name="price-radio-checkbox"
+					onChange={inputHandler}
+					checked={props.filterState.filter.priceDown}
+				></input>
+				<label htmlFor="price-down">Price down</label>
 			</li>
 		</>
 	);
