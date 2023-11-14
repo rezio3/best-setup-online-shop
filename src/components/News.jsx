221605890348af7bfb2ae25e879aae4fab87a6df
Joszko <jakub.rezler96@gmail.com>
2023-11-14 04:13:43 +0100

    create News.jsx component

---
+++ src/components/News.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../style/css/news.css";
+
+const News = () => {
+	return (
+		<>
+			<div className="news-container"></div>
+		</>
+	);
+};
+
+export default News;
