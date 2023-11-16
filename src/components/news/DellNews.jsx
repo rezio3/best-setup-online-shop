5dd1667e84ca4e3581c1751752c65b65736bfcf3
Joszko <jakub.rezler96@gmail.com>
2023-11-16 03:46:10 +0100

    update DellNews.jsx component

--- src/components/news/DellNews.jsx
+++ src/components/news/DellNews.jsx
@@ -1,11 +1,32 @@
 import React from "react";
+import "../../style/css/dellNews.css";
+import dell1 from "../../img/news/Alienware Aurora R16 Gaming Desktop.jpg";
 
 const DellNews = () => {
 	return (
 		<>
 			<div className="dell-news-scroller-container">
-				<div className="dell-news-scroller-container__dell-news-box"></div>
-				<div className="dell-news-scroller-container__dell-news-box"></div>
+				<div className="dell-news-box">
+					<div className="dell-news-box__image-container">
+						<img
+							src={dell1}
+							alt="Alienware Aurora R16 Gaming Desktop"
+							className="dell-news-box__image"
+						/>
+					</div>
+					<div className="dell-news-box__description-container">
+						<span className="dell-news-box__product-type-span">
+							PC | Gaming
+						</span>
+						<h4 className="dell-news-box__product-name">
+							Alienware Aurora R16 Gaming Desktop
+						</h4>
+						<p className="dell-news-box__product-description">
+							Refined and simplified, the Aurora R16 is designed to be a more
+							minimal and efficient desktop with improvements in every category.
+						</p>
+					</div>
+				</div>
 				<div className="dell-news-scroller-container__dell-news-box"></div>
 				<div className="dell-news-scroller-container__dell-news-box"></div>
 				<div className="dell-news-scroller-container__dell-news-box"></div>
