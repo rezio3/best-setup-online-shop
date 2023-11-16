894704d81178c760706777f0356fafdd91fc36da
Joszko <jakub.rezler96@gmail.com>
2023-11-16 03:09:54 +0100

    add DellNews component to News.jsx

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "../style/css/news.css";
 import NewsAdamAudio from "./news/NewsAdamAudio";
+import DellNews from "./news/DellNews";
 
 const News = () => {
 	return (
 		<div className="news-container">
 			<NewsAdamAudio />
+			<DellNews />
 		</div>
 	);
 };
