5a2415a367614a3f05c4f1ae268b777f58e615ed
Joszko <jakub.rezler96@gmail.com>
2023-11-16 01:30:55 +0100

    add navigatior-description to Home component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,6 +7,13 @@ import News from "../components/News";
 const Home = () => {
 	return (
 		<>
+			<div className="navigator-description-container">
+				<span className="navigator-description-container__arrow-span">
+					{" "}
+					&#8594;{" "}
+				</span>
+				<h2 className="navigator-description-container__header">Hot deals</h2>
+			</div>
 			<div className="main-home-page-container">
 				<FilterWindow />
 				<ProductsHomePage />
