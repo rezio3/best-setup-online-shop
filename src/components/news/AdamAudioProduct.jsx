a662d54dcfb6b2287690d3b1bc581a73587b84cb
Joszko <jakub.rezler96@gmail.com>
2023-11-16 02:35:47 +0100

    create separate component for AdamAudioProducts

---
+++ src/components/news/AdamAudioProduct.jsx
@@ -0,0 +1,37 @@
+import React, { useRef } from "react";
+import { gsap } from "gsap";
+
+const AdamAudioProduct = (props) => {
+	// DLACZEGO TEN USEREF NIE POBIERA ELEMENTU?! SKORO W PRODUCTBOX.JSX POBIERA BEZ PROBLEMU
+	let productImage = useRef(null);
+
+	const handleHover = () => {
+		gsap.to(productImage, { scale: 1.1, duration: 0.3, ease: "expo" });
+	};
+	const handleHoverOut = () => {
+		gsap.to(productImage, { scale: 1, duration: 0.3, ease: "expo" });
+	};
+
+	return (
+		<>
+			<div
+				className="adam-audio-container__image-box adam-audio-img-box"
+				onMouseEnter={handleHover}
+				onMouseLeave={handleHoverOut}
+			>
+				<img
+					src={props.image}
+					id={props.name}
+					className="adam-audio-img-box__adam-image"
+					ref={(el) => (productImage = el)}
+				/>
+				<h4 className="adam-audio-img-box__header">{props.name}</h4>
+				<span className="adam-audio-img-box__description">
+					{props.description}
+				</span>
+			</div>
+		</>
+	);
+};
+
+export default AdamAudioProduct;
