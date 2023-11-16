17cec95d323b902bda213de63dbe77c9945e4565
Joszko <jakub.rezler96@gmail.com>
2023-11-16 03:09:36 +0100

    create DellNews.jsx component

---
+++ src/components/news/DellNews.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const DellNews = () => {
+	return (
+		<>
+			<div className="dell-news-scroller-container">
+				<div className="dell-news-scroller-container__dell-news-box"></div>
+				<div className="dell-news-scroller-container__dell-news-box"></div>
+				<div className="dell-news-scroller-container__dell-news-box"></div>
+				<div className="dell-news-scroller-container__dell-news-box"></div>
+				<div className="dell-news-scroller-container__dell-news-box"></div>
+				<div className="dell-news-scroller-container__dell-news-box"></div>
+				<div className="dell-news-scroller-container__dell-news-box"></div>
+				<div className="dell-news-scroller-container__dell-news-box"></div>
+				<div className="dell-news-scroller-container__dell-news-box"></div>
+				<div className="dell-news-scroller-container__dell-news-box"></div>
+			</div>
+		</>
+	);
+};
+
+export default DellNews;
