51856cc062a9d2895346e113c4441c3ab0d743ba
Joszko <jakub.rezler96@gmail.com>
2023-11-16 02:35:33 +0100

    create separate component for AdamAudioDescription

---
+++ src/components/news/AdamAudioDescription.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const AdamAudioDescription = () => {
+	return (
+		<>
+			<p className="adam-audio-container__description">
+				The ADAM Audio A Series delivers highly accurate, transparent sound
+				across a full line of monitors that can be controlled remotely and in
+				real time. Comprising five models, the A4V, A7V, A44H, A77H and A8H are
+				the right fit for an array of professional and home studios—as well as
+				broadcast, fixed-installation, 3D, immersive, and home audio
+				environments.
+			</p>
+			<p className="adam-audio-container__description">
+				ADAM Audio monitors have earned a worldwide reputation as the
+				pre-eminent tool for sound engineers based on our technological
+				innovations in the field of loudspeaker technology. These advances have
+				helped to define the immediately identifiable sonic characteristics of
+				our loudspeakers: accurate and transparent, with high definition.
+			</p>
+		</>
+	);
+};
+
+export default AdamAudioDescription;
