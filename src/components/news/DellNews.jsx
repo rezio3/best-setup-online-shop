98bb40bb130cbcc3b448f6bd8a17970022b1eed9
Joszko <jakub.rezler96@gmail.com>
2023-11-17 02:08:54 +0100

    add new boxes to DellNews.jsx component

--- src/components/news/DellNews.jsx
+++ src/components/news/DellNews.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import "../../style/css/dellNews.css";
 import dell1 from "../../img/news/Alienware Aurora R16 Gaming Desktop.jpg";
+import dell2 from "../../img/news/PS5 Console - Marvel's Spider-Man 2 Bundle.jpg";
+import dell3 from "../../img/news/Alienware 34 Curved QD-OLED Gaming Monitor - AW3424DWF.jpg";
 
 const DellNews = () => {
 	return (
 		<>
 			<div className="dell-news-scroller-container">
+				{/* --------------------------------- 1 ------------------------ */}
 				<div className="dell-news-box">
 					<div className="dell-news-box__image-container">
 						<img
@@ -14,6 +17,7 @@ const DellNews = () => {
 							className="dell-news-box__image"
 						/>
 					</div>
+
 					<div className="dell-news-box__description-container">
 						<span className="dell-news-box__product-type-span">
 							PC | Gaming
@@ -27,14 +31,55 @@ const DellNews = () => {
 						</p>
 					</div>
 				</div>
-				<div className="dell-news-scroller-container__dell-news-box"></div>
-				<div className="dell-news-scroller-container__dell-news-box"></div>
-				<div className="dell-news-scroller-container__dell-news-box"></div>
-				<div className="dell-news-scroller-container__dell-news-box"></div>
-				<div className="dell-news-scroller-container__dell-news-box"></div>
-				<div className="dell-news-scroller-container__dell-news-box"></div>
-				<div className="dell-news-scroller-container__dell-news-box"></div>
-				<div className="dell-news-scroller-container__dell-news-box"></div>
+				{/* ----------------------------- 2 ---------------------------- */}
+				<div className="dell-news-box">
+					<div className="dell-news-box__image-container">
+						<img
+							src={dell2}
+							alt="Alienware 34 Curved QD-OLED Gaming Monitor - AW3424DWF"
+							className="dell-news-box__image"
+						/>
+					</div>
+
+					<div className="dell-news-box__description-container">
+						<span className="dell-news-box__product-type-span">
+							Console | PS
+						</span>
+						<h4 className="dell-news-box__product-name">
+							Alienware 34 Curved QD-OLED Gaming Monitor - AW3424DWF
+						</h4>
+						<p className="dell-news-box__product-description">
+							Experience Marvel’s Spider-Man 2 with PS5 innovations. Feel the
+							force of web-swinging with adaptive triggers, while haptic
+							feedback responds to Peter Parker’s new symbiote abilities and
+							Miles Morales’ evolved bio-electric skills.
+						</p>
+					</div>
+				</div>
+				{/* ------------------------------- 3 ---------------------------- */}
+				<div className="dell-news-box">
+					<div className="dell-news-box__image-container">
+						<img
+							src={dell3}
+							alt="PS5 Console - Marvel's Spider-Man 2 Bundle"
+							className="dell-news-box__image"
+						/>
+					</div>
+
+					<div className="dell-news-box__description-container">
+						<span className="dell-news-box__product-type-span">
+							Monitor | Ultrawide
+						</span>
+						<h4 className="dell-news-box__product-name">
+							PS5 Console - Marvel's Spider-Man 2 Bundle
+						</h4>
+						<p className="dell-news-box__product-description">
+							34" curved monitor with QD-OLED technology and AMD FreeSync™
+							Premium Pro. Featuring infinite contrast ratio and VESA DisplayHDR
+							TrueBlack 400 for an incredible front-of-screen performance.
+						</p>
+					</div>
+				</div>
 			</div>
 		</>
 	);
