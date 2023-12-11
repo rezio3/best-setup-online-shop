babb98a8017189d9b1c8656824d70bde1a211e84
Joszko <jakub.rezler96@gmail.com>
2023-12-11 01:03:59 +0100

    add price range handler to FilterWindow.jsx

--- src/components/FilterWindow.jsx
+++ src/components/FilterWindow.jsx
@@ -38,6 +38,26 @@ const FilterWindow = () => {
 		{ name: "Comfort", id: "comfort" },
 	];
 
+	const handlePriceRangeInputs = (e) => {
+		if (e.target.id === "price-from-input") {
+			setFilter({
+				...filter,
+				priceRange: {
+					...filter.priceRange,
+					priceFrom: e.target.value,
+				},
+			});
+		} else if (e.target.id === "price-to-input") {
+			setFilter({
+				...filter,
+				priceRange: {
+					...filter.priceRange,
+					priceTo: e.target.value,
+				},
+			});
+		}
+	};
+
 	return (
 		<>
 			<div className="filter-snap-threshold" ref={filterRef}></div>
@@ -65,9 +85,17 @@ const FilterWindow = () => {
 					</span>
 					<div className="filter-container__price-range-inputs-container">
 						<span>From: </span>
-						<input type="number"></input>
+						<input
+							type="number"
+							id="price-from-input"
+							onChange={handlePriceRangeInputs}
+						></input>
 						<span> To: </span>
-						<input type="number"></input>
+						<input
+							type="number"
+							id="price-to-input"
+							onChange={handlePriceRangeInputs}
+						></input>
 					</div>
 					<ul>
 						<FilterCheckboxPriceDirection filterState={{ filter, setFilter }} />
