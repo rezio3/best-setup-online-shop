e0fad85fe275f4b1f936855bea094307413e65bd
Joszko <jakub.rezler96@gmail.com>
2023-12-12 09:59:20 +0100

    add Display component

--- src/layouts/Page.jsx
+++ src/layouts/Page.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Home from "../pages/Home";
 import FilterProductsContext from "../context/FilterContext";
+import Display from "../pages/Display";
 
 const Page = () => {
 	return (
 		<>
 			<FilterProductsContext>
 				<Home />
+				<Display />
 			</FilterProductsContext>
 		</>
 	);
