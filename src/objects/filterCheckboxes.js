a6fbfe6bb5baa3b920321b7efd086f36b336675e
Joszko <jakub.rezler96@gmail.com>
2023-12-18 04:52:32 +0100

    update filterCheckboxes.js

--- src/objects/filterCheckboxes.js
+++ src/objects/filterCheckboxes.js
@@ -7,17 +7,24 @@ export const homePageCheckboxesNames = [
 ];
 
 export const displayPageCheckboxesNames = [
-	{ name: "Full HD", id: "Full HD" },
-	{ name: "2k", id: "2k" },
-	{ name: "4k", id: "4k" },
-	{ name: "5k", id: "5k" },
-	{ name: "Ultrawide", id: "Ultrawide" },
-	{ name: "60 FPS", id: "60fps" },
-	{ name: "100 FPS", id: "100fps" },
-	{ name: "120 FPS", id: "120fps" },
-	{ name: `24"`, id: `24"` },
-	{ name: `25"`, id: `25"` },
-	{ name: `26"`, id: `26"` },
-	{ name: `27"`, id: `27"` },
-	{ name: `28"`, id: `28"` },
+	[
+		{ name: "Full HD", id: "Full HD" },
+		{ name: "2k", id: "2k" },
+		{ name: "4k", id: "4k" },
+		{ name: "5k", id: "5k" },
+		{ name: "Ultrawide", id: "Ultrawide" },
+	],
+	[
+		{ name: "60 FPS", id: "60fps" },
+		{ name: "100 FPS", id: "100fps" },
+		{ name: "120 FPS", id: "120fps" },
+	],
+
+	[
+		{ name: `24"`, id: `24"` },
+		{ name: `25"`, id: `25"` },
+		{ name: `26"`, id: `26"` },
+		{ name: `27"`, id: `27"` },
+		{ name: `28"`, id: `28"` },
+	],
 ];
