f4e2d996a3d0ed2c2af2bd1dddb5931d6290af3b
Joszko <jakub.rezler96@gmail.com>
2023-12-19 04:48:25 +0100

    create sound page

--- src/pages/Sound.jsx
+++ src/pages/Sound.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import FilterWindow from "../components/FilterWindow";
+import ProductsSoundPage from "../components/soundpage/ProductsSoundPage";
 
 const Sound = () => {
 	return (
@@ -9,6 +11,10 @@ const Sound = () => {
 					&#8594;{" "}
 				</span>
 				<h2 className="navigator-description-container__header">Sound</h2>
+			</div>
+			<div className="main-product-container">
+				<FilterWindow page={"sounds-page"} />
+				<ProductsSoundPage />
 			</div>
 		</>
 	);
