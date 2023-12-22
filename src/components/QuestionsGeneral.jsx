480be5dc421e63df32ef91f564dd53ac1c0fb6b4
Joszko <jakub.rezler96@gmail.com>
2023-12-22 06:57:08 +0100

    update answers in QuestionsGeneral.jsx component

--- src/components/QuestionsGeneral.jsx
+++ src/components/QuestionsGeneral.jsx
@@ -14,29 +14,29 @@ const QuestionsGeneral = () => {
 			question:
 				"Is there a minimum purchase amount to take advantage of the 0% installment offer?",
 			answer:
-				"To use equipment financing with a voucher, simply choose the financing option at checkout, and follow the prompts to complete the process.",
+				"Indeed, there is a minimum purchase amount required to take advantage of the 0% installment offer, ensuring eligibility for this financing option.",
 		},
 		{
 			question:
 				"What is the financing period for equipment using 0% installments?",
 			answer:
-				"To use equipment financing with a voucher, simply choose the financing option at checkout, and follow the prompts to complete the process.",
+				"The financing period for equipment using 0% installments varies based on the specific terms offered during the checkout process, providing flexibility to meet your needs.",
 		},
 		{
 			question: "Can I use other payment methods along with voucher financing?",
 			answer:
-				"To use equipment financing with a voucher, simply choose the financing option at checkout, and follow the prompts to complete the process.",
+				"Certainly, you can combine voucher financing with other payment methods, allowing you to tailor your payment approach according to your preferences.",
 		},
 		{
 			question:
 				"Does the 0% installment offer cover all products available in the store?",
 			answer:
-				"To use equipment financing with a voucher, simply choose the financing option at checkout, and follow the prompts to complete the process.",
+				"The 0% installment offer extends to a diverse selection of products available in the store, offering flexibility and accessibility for various purchases.",
 		},
 		{
 			question: "What documents are required to use voucher financing?",
 			answer:
-				"To use equipment financing with a voucher, simply choose the financing option at checkout, and follow the prompts to complete the process.",
+				"To utilize voucher financing, typically you'll need to provide standard identification documents and any additional details specified during the checkout process, streamlining the financing application.",
 		},
 	];
 	return (
