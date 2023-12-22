04c5f0c004642bc73114e8f32a27ac61b73c692f
Joszko <jakub.rezler96@gmail.com>
2023-12-22 02:09:27 +0100

    create InstallmentOffer.jsx component

---
+++ src/components/InstallmentOffer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../style/css/installmentOffer.css";
+
+const InstallmentOffer = () => {
+	return (
+		<>
+			<div className="installment-offer-section"></div>
+		</>
+	);
+};
+
+export default InstallmentOffer;
