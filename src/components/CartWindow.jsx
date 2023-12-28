07c3b416164f4732ebe15d5faac34a2179fa3dd2
Joszko <jakub.rezler96@gmail.com>
2023-12-29 00:37:21 +0100

    create basic cartWindow styles

--- src/components/CartWindow.jsx
+++ src/components/CartWindow.jsx
@@ -1,7 +1,16 @@
-import React from "react";
+import React, { useContext } from "react";
+import "../style/css/cartWindow.css";
+import { ProductsOrderContext } from "../context/OrderContext";
 
 const CartWindow = () => {
-	return <></>;
+	const [order, setOrder] = useContext(ProductsOrderContext);
+
+	console.log(order);
+	return (
+		<div className="cart-window-wrapper">
+			<div className="cart-window-container"></div>
+		</div>
+	);
 };
 
 export default CartWindow;
