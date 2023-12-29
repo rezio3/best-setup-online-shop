064925feaa8605b639f1e551133c7aca37b8da2c
Joszko <jakub.rezler96@gmail.com>
2023-12-29 05:40:31 +0100

    create custom scroll in cart window

--- src/components/cartwindow/CartWindow.jsx
+++ src/components/cartwindow/CartWindow.jsx
@@ -2,13 +2,14 @@ import React, { useContext } from "react";
 import "../../style/css/cartWindow.css";
 import { ProductsOrderContext } from "../../context/OrderContext";
 import ItemInCart from "./ItemInCart";
+import "../../style/css/scrollCustom.css";
 
 const CartWindow = () => {
 	const [order, setOrder] = useContext(ProductsOrderContext);
 	return (
 		<>
 			{order.cartOpen ? (
-				<div className="cart-window-wrapper">
+				<div className="cart-window-wrapper cart-window-scroll">
 					<div className="cart-window-container">
 						<h3 className="cart-window-container__header">Your Cart</h3>
 						{order.cart.map((e) => {
