b11124fd5cc4c13ad45c86ae19c5984315708a14
Joszko <jakub.rezler96@gmail.com>
2023-12-29 01:15:08 +0100

    create ItemInCart.jsx component

--- src/components/cartwindow/CartWindow.jsx
+++ src/components/cartwindow/CartWindow.jsx
@@ -1,6 +1,7 @@
 import React, { useContext } from "react";
-import "../style/css/cartWindow.css";
-import { ProductsOrderContext } from "../context/OrderContext";
+import "../../style/css/cartWindow.css";
+import { ProductsOrderContext } from "../../context/OrderContext";
+import ItemInCart from "./ItemInCart";
 
 const CartWindow = () => {
 	const [order, setOrder] = useContext(ProductsOrderContext);
@@ -10,7 +11,10 @@ const CartWindow = () => {
 		<>
 			{order.cartOpen ? (
 				<div className="cart-window-wrapper">
-					<div className="cart-window-container"></div>
+					<div className="cart-window-container">
+						<h3 className="cart-window-container__header">Your Cart</h3>
+						<ItemInCart />
+					</div>
 				</div>
 			) : null}
 		</>
---
+++ src/components/cartwindow/ItemInCart.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ItemInCart = () => {
+	return (
+		<>
+			<div className="cart-window-container__item-box"></div>
+		</>
+	);
+};
+
+export default ItemInCart;
