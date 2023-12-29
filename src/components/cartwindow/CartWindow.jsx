4e0c93f04e7a1fc571c8c5cf43fb7e559847747e
Joszko <jakub.rezler96@gmail.com>
2023-12-29 06:04:20 +0100

    add back button to cartWindow.jsx component

--- src/components/cartwindow/CartWindow.jsx
+++ src/components/cartwindow/CartWindow.jsx
@@ -3,18 +3,39 @@ import "../../style/css/cartWindow.css";
 import { ProductsOrderContext } from "../../context/OrderContext";
 import ItemInCart from "./ItemInCart";
 import "../../style/css/scrollCustom.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faXmark } from "@fortawesome/free-solid-svg-icons";
 
 const CartWindow = () => {
 	const [order, setOrder] = useContext(ProductsOrderContext);
+	const cartXButtonHandler = (event) => {
+		event.target === event.currentTarget &&
+			setOrder({
+				...order,
+				cartOpen: !order.cartOpen,
+			});
+	};
 	return (
 		<>
 			{order.cartOpen ? (
-				<div className="cart-window-wrapper cart-window-scroll">
-					<div className="cart-window-container">
-						<h3 className="cart-window-container__header">Your Cart</h3>
-						{order.cart.map((e) => {
-							return <ItemInCart image={e.img} name={e.name} price={e.price} />;
-						})}
+				<div className="cart-window-background" onClick={cartXButtonHandler}>
+					<div className="cart-window-wrapper cart-window-scroll">
+						<div className="cart-window-container">
+							<div className="cart-window-container__header-box">
+								<h3 className="cart-window-container__header">Your Cart</h3>
+								<button
+									className="cart-window-container__back-btn"
+									onClick={cartXButtonHandler}
+								>
+									<FontAwesomeIcon icon={faXmark} />
+								</button>
+							</div>
+							{order.cart.map((e) => {
+								return (
+									<ItemInCart image={e.img} name={e.name} price={e.price} />
+								);
+							})}
+						</div>
 					</div>
 				</div>
 			) : null}
