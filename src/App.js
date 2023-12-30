b366626e5aa4bd0f946c1b14b0684f2b9a1d49e9
Joszko <jakub.rezler96@gmail.com>
2023-12-30 17:30:25 +0100

    add temporary "in progress" window

--- src/App.js
+++ src/App.js
@@ -6,8 +6,14 @@ import Navbar from "./layouts/Navbar";
 import Page from "./layouts/Page";
 import OrderContext from "./context/OrderContext";
 import CartWindow from "./components/cartwindow/CartWindow";
+import { useState } from "react";
+import "./style/css/buttonsAnim.css";
 
 function App() {
+	const [ok, setOk] = useState(true);
+	const okBtnHandler = () => {
+		setOk(false);
+	};
 	return (
 		<Router>
 			<div className="App">
@@ -18,6 +24,23 @@ function App() {
 					<Footer />
 					<CartWindow />
 				</OrderContext>
+				<div
+					className={
+						ok ? "in-progress-box" : "in-progress-box in-progress-box--off"
+					}
+				>
+					<h2>This shop ain't work yet!</h2>
+					<span className="in-progress-span">
+						Project is currently in progress...
+					</span>
+					<span className="in-progress-span">
+						So far, you can browse products, filter them and add them to the
+						cart.
+					</span>
+					<button className="ok-btn button-anim" onClick={okBtnHandler}>
+						Ok
+					</button>
+				</div>
 			</div>
 		</Router>
 	);
