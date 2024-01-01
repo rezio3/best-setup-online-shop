4a6bd861082eb4c5ce1cdbda4bbe5810b9465b04
Joszko <jakub.rezler96@gmail.com>
2024-01-01 21:00:02 +0100

    add ScrollToTop.jsx component

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import OrderContext from "./context/OrderContext";
 import CartWindow from "./components/cartwindow/CartWindow";
 import { useState } from "react";
 import "./style/css/buttonsAnim.css";
+import ScrollToTop from "./components/ScrollToTop";
 
 function App() {
 	const [ok, setOk] = useState(true);
@@ -16,32 +17,34 @@ function App() {
 	};
 	return (
 		<Router>
-			<div className="App">
-				<OrderContext>
-					<Header />
-					<Navbar />
-					<Page />
-					<Footer />
-					<CartWindow />
-				</OrderContext>
-				<div
-					className={
-						ok ? "in-progress-box" : "in-progress-box in-progress-box--off"
-					}
-				>
-					<h2>This shop ain't working yet!</h2>
-					<span className="in-progress-span">
-						Project is currently in progress...
-					</span>
-					<span className="in-progress-span">
-						So far, you can browse products, filter them and add them to the
-						cart.
-					</span>
-					<button className="ok-btn button-anim" onClick={okBtnHandler}>
-						Ok
-					</button>
+			<ScrollToTop>
+				<div className="App">
+					<OrderContext>
+						<Header />
+						<Navbar />
+						<Page />
+						<Footer />
+						<CartWindow />
+					</OrderContext>
+					<div
+						className={
+							ok ? "in-progress-box" : "in-progress-box in-progress-box--off"
+						}
+					>
+						<h2>This shop ain't working yet!</h2>
+						<span className="in-progress-span">
+							Project is currently in progress...
+						</span>
+						<span className="in-progress-span">
+							So far, you can browse products, filter them and add them to the
+							cart.
+						</span>
+						<button className="ok-btn button-anim" onClick={okBtnHandler}>
+							Ok
+						</button>
+					</div>
 				</div>
-			</div>
+			</ScrollToTop>
 		</Router>
 	);
 }
