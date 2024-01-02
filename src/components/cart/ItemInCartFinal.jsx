e0cfecafef40958e89d8533ef4173263ebdea775
Joszko <jakub.rezler96@gmail.com>
2024-01-02 02:57:20 +0100

    add style modifications to ItemInCartFinal.jsx component

--- src/components/cart/ItemInCartFinal.jsx
+++ src/components/cart/ItemInCartFinal.jsx
@@ -22,21 +22,24 @@ const ItemInCartFinal = (props) => {
 	};
 	return (
 		<>
-			<div className="cart-window-container__item-box">
-				<img src={props.image} className="cart-window-container__item-img" />
+			<div className="cart-window-container__item-box item-box-final">
+				<img
+					src={props.image}
+					className="cart-window-container__item-img item-box-final__item-img"
+				/>
 				<span className="cart-window-container__item-name">{props.name}</span>
 				<QuantityBox directItem={directItem} objIndex={objIndex} />
 
-				<span className="cart-window-container__item-price">
+				<span className="cart-window-container__item-price item-box-final__item-price">
 					{props.price} $
 				</span>
 				<button
-					className="cart-window-container__trash-button"
+					className="cart-window-container__trash-button item-box-final__trash-button"
 					onClick={trashHandler}
 				>
 					<FontAwesomeIcon
 						icon={faTrash}
-						className="cart-window-container__trash-icon"
+						className="cart-window-container__trash-icon item-box-final__trash-icon"
 					/>
 				</button>
 			</div>
