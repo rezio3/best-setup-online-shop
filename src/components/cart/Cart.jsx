62725010425923dbb67f09bec9b19c3b57a99e50
Joszko <jakub.rezler96@gmail.com>
2024-01-02 01:49:45 +0100

    update Cart.jsx

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -1,5 +1,6 @@
 import React, { useContext } from "react";
 import { ProductsOrderContext } from "../../context/OrderContext";
+import "../../style/css/cart.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTrash } from "@fortawesome/free-solid-svg-icons";
 
@@ -10,14 +11,17 @@ const Cart = () => {
 			<div className="cart-wrapper">
 				<div className="cart-container">
 					<div className="cart-container__header-box">
-						<h3>
-							Cart <span>({order.getProductsQuantity()})</span>
+						<h3 className="cart-container__header">
+							Cart{" "}
+							<span className="cart-container__header--gray">
+								({order.getProductsQuantity()})
+							</span>
 						</h3>
-						<button>
+						<button className="cart-container__clear-cart-btn">
 							Clear cart
 							<FontAwesomeIcon
 								icon={faTrash}
-								className="cart-window-container__trash-icon"
+								className="cart-container__trash-icon"
 							/>
 						</button>
 					</div>
