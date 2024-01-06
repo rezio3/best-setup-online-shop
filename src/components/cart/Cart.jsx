4dd1b37978b80cd0f23397465e52792e52b40947
Joszko <jakub.rezler96@gmail.com>
2024-01-06 11:33:28 +0100

    update Cart.jsx

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -19,39 +19,43 @@ const Cart = () => {
 	return (
 		<>
 			<div className="cart-wrapper">
-				<div className="cart-container">
-					<div className="cart-container__header-box">
-						<h3 className="cart-container__header">
-							Cart{" "}
-							<span className="cart-container__header--gray">
-								({order.getProductsQuantity()})
-							</span>
-						</h3>
-						<button
-							className="cart-container__clear-cart-btn"
-							onClick={deleteCartHandler}
-						>
-							Clear cart
-							<FontAwesomeIcon
-								icon={faTrash}
-								className="cart-container__trash-icon"
-							/>
-						</button>
-					</div>
-					<div className="cart-box">
-						{order.cart.map((e) => {
-							return (
-								<ItemInCartFinal
-									image={e.img}
-									name={e.name}
-									price={e.price}
-									key={e.name}
-								/>
-							);
-						})}
-					</div>
-				</div>
-				<div className="go-to-order-form-container"></div>
+				{order.cart.length !== 0 ? (
+					<>
+						<div className="cart-container">
+							<div className="cart-container__header-box">
+								<h3 className="cart-container__header">
+									Cart{" "}
+									<span className="cart-container__header--gray">
+										({order.getProductsQuantity()})
+									</span>
+								</h3>
+								<button
+									className="cart-container__clear-cart-btn"
+									onClick={deleteCartHandler}
+								>
+									Clear cart
+									<FontAwesomeIcon
+										icon={faTrash}
+										className="cart-container__trash-icon"
+									/>
+								</button>
+							</div>
+							<div className="cart-box">
+								{order.cart.map((e) => {
+									return (
+										<ItemInCartFinal
+											image={e.img}
+											name={e.name}
+											price={e.price}
+											key={e.name}
+										/>
+									);
+								})}
+							</div>
+						</div>
+						<div className="go-to-order-form-container"></div>
+					</>
+				) : null}
 			</div>
 		</>
 	);
