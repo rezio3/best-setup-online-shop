9694464baff78deff56db5a99bbe49374b75b232
Joszko <jakub.rezler96@gmail.com>
2024-01-08 06:02:14 +0100

    create random generator of recommended products

--- src/components/cart/FinalCartLowerBox.jsx
+++ src/components/cart/FinalCartLowerBox.jsx
@@ -3,7 +3,22 @@ import "../../style/css/finalCartLowerBox.css";
 import GuaranteeBox from "./GuaranteeBox";
 import guaranteeBoxesArray from "../../objects/guaranteeDelivery";
 
+import products from "../../objects/products";
+import RecommendedProduct from "./RecommendedProduct";
+
 const FinalCartLowerBox = () => {
+	const limitRandomize = products.hotDeals.length - 1;
+	const recommendedProductsArray = [];
+
+	do {
+		const randomNumber = Math.floor(Math.random() * limitRandomize);
+		const selectedProduct = products.hotDeals[randomNumber];
+
+		if (!recommendedProductsArray.includes(selectedProduct)) {
+			recommendedProductsArray.push(selectedProduct);
+		}
+	} while (recommendedProductsArray.length < 10);
+
 	return (
 		<>
 			<div className="final-cart-lower-box-wrapper">
@@ -17,6 +32,16 @@ const FinalCartLowerBox = () => {
 							/>
 						);
 					})}
+				</div>
+				<div className="recommended-products-container">
+					<h3 className="recommended-products-container__header">
+						Recommended products
+					</h3>
+					<div className="recommended-products-carousel">
+						{recommendedProductsArray.map((e) => (
+							<RecommendedProduct product={e} />
+						))}
+					</div>
 				</div>
 			</div>
 		</>
