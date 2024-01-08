76a9175207c78f61b69ef6beb6615872717d43d3
Joszko <jakub.rezler96@gmail.com>
2024-01-08 04:15:57 +0100

    create separate object for guaranteeDelivery array

--- src/components/cart/FinalCartLowerBox.jsx
+++ src/components/cart/FinalCartLowerBox.jsx
@@ -1,28 +1,9 @@
 import React from "react";
-import icon1 from "../../img/icon1.png";
-import icon2 from "../../img/icon2.png";
-import icon3 from "../../img/icon3.png";
 import "../../style/css/finalCartLowerBox.css";
 import GuaranteeBox from "./GuaranteeBox";
+import guaranteeBoxesArray from "../../objects/guaranteeDelivery";
 
 const FinalCartLowerBox = () => {
-	const guaranteeBoxesArray = [
-		{
-			firstText: "Secure Shopping",
-			secondText: "Thanks to TLS Encryption",
-			img: icon1,
-		},
-		{
-			firstText: "Free Returns Within 15 Days",
-			secondText: "Via Parcel Locker or at Our Stores",
-			img: icon2,
-		},
-		{
-			firstText: "Technical Support",
-			secondText: "Email, Phone, and Chat",
-			img: icon3,
-		},
-	];
 	return (
 		<>
 			<div className="final-cart-lower-box-wrapper">
---
+++ src/objects/guaranteeDelivery.js
@@ -0,0 +1,23 @@
+import icon1 from "../img/icon1.png";
+import icon2 from "../img/icon2.png";
+import icon3 from "../img/icon3.png";
+
+const guaranteeBoxesArray = [
+	{
+		firstText: "Secure Shopping",
+		secondText: "Thanks to TLS Encryption",
+		img: icon1,
+	},
+	{
+		firstText: "Free Returns Within 15 Days",
+		secondText: "Via Parcel Locker or at Our Stores",
+		img: icon2,
+	},
+	{
+		firstText: "Technical Support",
+		secondText: "Email, Phone, and Chat",
+		img: icon3,
+	},
+];
+
+export default guaranteeBoxesArray;
