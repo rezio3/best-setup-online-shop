dfaa9fe94e1da782d1767a660719d20f65e85a9b
Joszko <jakub.rezler96@gmail.com>
2024-01-09 04:48:03 +0100

    update classnames in delivery page

--- src/components/orderform/CustomerDataInput.jsx
+++ src/components/orderform/CustomerDataInput.jsx
@@ -3,9 +3,9 @@ import React from "react";
 const CustomerDataInput = (props) => {
 	return (
 		<>
-			<label className="personal-data-container__label">
+			<label className="data-container__label">
 				{props.label}
-				<input type="text" className="personal-data-container__input" />
+				<input type="text" className="data-container__input" />
 			</label>
 		</>
 	);
--- src/components/orderform/PersonalData.jsx
+++ src/components/orderform/PersonalData.jsx
@@ -4,16 +4,14 @@ import CustomerDataInput from "./CustomerDataInput";
 const PersonalData = () => {
 	return (
 		<>
-			<div className="personal-data-container">
-				<h3 className="personal-data-container__header">
-					Personal data & contact
-				</h3>
-				<div className="personal-data-container__inputs-container">
-					<div className="personal-data-container__inputs-box">
+			<div className="data-container">
+				<h3 className="data-container__header">Personal data & contact</h3>
+				<div className="data-container__inputs-container">
+					<div className="data-container__inputs-box">
 						<CustomerDataInput label="Name" />
 						<CustomerDataInput label="Surname" />
 					</div>
-					<div className="personal-data-container__inputs-box">
+					<div className="data-container__inputs-box">
 						<CustomerDataInput label="Phone" />
 						<CustomerDataInput label="Email" />
 					</div>
