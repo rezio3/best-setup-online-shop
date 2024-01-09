b0ffcdcfe02ec069af9e519d79c16a9b1357d08f
Joszko <jakub.rezler96@gmail.com>
2024-01-09 04:48:20 +0100

    update DeliveryAddress.jsx

--- src/components/orderform/DeliveryAddress.jsx
+++ src/components/orderform/DeliveryAddress.jsx
@@ -1,7 +1,28 @@
 import React from "react";
+import CustomerDataInput from "./CustomerDataInput";
 
 const DeliveryAddress = () => {
-	return <></>;
+	return (
+		<>
+			<div className="data-container">
+				<h3 className="data-container__header">Delivery address</h3>
+				<div className="data-container__inputs-container">
+					<div className="data-container__inputs-box">
+						<CustomerDataInput label="City" />
+						<CustomerDataInput label="Postal code" />
+					</div>
+					<div className="data-container__inputs-box">
+						<CustomerDataInput label="Street" />
+						<CustomerDataInput label="Building number" />
+					</div>
+					<div className="data-container__inputs-box">
+						<CustomerDataInput label="Flat number" />
+						{/* <CustomerDataInput label="Building number" /> */}
+					</div>
+				</div>
+			</div>
+		</>
+	);
 };
 
 export default DeliveryAddress;
