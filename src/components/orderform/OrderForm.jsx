e1cfec7b7656c19b5159a8bb57ab498bd05de71a
Joszko <jakub.rezler96@gmail.com>
2024-01-09 05:26:51 +0100

    add back btn to OrderForm.jsx component

--- src/components/orderform/OrderForm.jsx
+++ src/components/orderform/OrderForm.jsx
@@ -3,10 +3,19 @@ import "../../style/css/orderForm.css";
 import PersonalData from "./PersonalData";
 import TotalPriceWindow from "../TotalPriceWindow";
 import DeliveryAddress from "./DeliveryAddress";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAngleLeft } from "@fortawesome/free-solid-svg-icons";
 
 const OrderForm = () => {
 	return (
 		<>
+			<div className="back-btn-container">
+				<button className="back-btn-container__back-btn button-anim">
+					<FontAwesomeIcon icon={faAngleLeft} className="arrow" />
+					Edit cart
+				</button>
+			</div>
+
 			<div className="order-form-page">
 				<div className="order-form-container">
 					<PersonalData />
