c14e5402dde10cbb009f9533ade6f33740dd4ffe
Joszko <jakub.rezler96@gmail.com>
2024-01-09 02:01:54 +0100

    add EmptyCartFinal.jsx component

--- src/components/cart/CartPage.jsx
+++ src/components/cart/CartPage.jsx
@@ -3,13 +3,14 @@ import CartFinal from "./CartFinal";
 import "../../style/css/cartPage.css";
 import { ProductsOrderContext } from "../../context/OrderContext";
 import FinalCartLowerBox from "./FinalCartLowerBox";
+import EmptyCartFinal from "./EmptyCartFinal";
 
 const CartPage = () => {
 	const [order, setOrder] = useContext(ProductsOrderContext);
 	return (
 		<>
 			<div className="cart-page-wrapper">
-				{order.cart.length !== 0 ? <CartFinal /> : null}
+				{order.cart.length !== 0 ? <CartFinal /> : <EmptyCartFinal />}
 				<FinalCartLowerBox />
 			</div>
 		</>
---
+++ src/components/cart/EmptyCartFinal.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const EmptyCartFinal = () => {
+	return (
+		<>
+			<div className="empty-cart-final-container">
+				<h4 className="empty-cart-final-container__header">
+					Your cart is empty.
+				</h4>
+				<span className="empty-cart-final-container__inspiration-span">
+					Looking for inspiration?
+				</span>
+			</div>
+		</>
+	);
+};
+
+export default EmptyCartFinal;
