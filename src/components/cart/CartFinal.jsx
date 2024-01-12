ab1137d6274260668d94a360d57af38e5b24a688
Joszko <jakub.rezler96@gmail.com>
2024-01-12 01:30:28 +0100

    add deleting mechanics of insurances

--- src/components/cart/CartFinal.jsx
+++ src/components/cart/CartFinal.jsx
@@ -14,6 +14,7 @@ const CartFinal = () => {
 		setOrder({
 			...order,
 			cart: [],
+			insurances: [],
 		});
 	};
 
--- src/functions/deleteItemFromCart.js
+++ src/functions/deleteItemFromCart.js
@@ -2,9 +2,13 @@ const deleteItem = (order, setOrder, props) => {
 	const updatedCart = order.cart.filter((e) => {
 		return e.name !== props.name && e;
 	});
+	const updatedInsurance = order.insurances.filter((e) => {
+		return e.product !== props.name && e;
+	});
 	setOrder({
 		...order,
 		cart: updatedCart,
+		insurances: updatedInsurance,
 	});
 };
 
