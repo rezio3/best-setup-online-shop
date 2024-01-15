e93ffa8e74e4f6a7e833609aff867cc7b94c4b2d
Joszko <jakub.rezler96@gmail.com>
2024-01-15 23:01:57 +0100

    manage context state with customer data inputs

--- src/components/orderform/CustomerDataInput.jsx
+++ src/components/orderform/CustomerDataInput.jsx
@@ -1,11 +1,28 @@
-import React from "react";
+import React, { useContext } from "react";
+import { ProductsOrderContext } from "../../context/OrderContext";
 
 const CustomerDataInput = (props) => {
+	const [order, setOrder] = useContext(ProductsOrderContext);
+	const inputHandler = (e) => {
+		setOrder({
+			...order,
+			customer: {
+				...order.customer,
+				[props.inputName]: e.target.value,
+			},
+		});
+		// console.log(e.target.value);
+	};
 	return (
 		<>
 			<label className="data-container__label">
 				{props.label}
-				<input type="text" className="data-container__input" />
+				<input
+					type="text"
+					className="data-container__input"
+					onChange={inputHandler}
+					value={order.customer[props.inputName]}
+				/>
 			</label>
 		</>
 	);
