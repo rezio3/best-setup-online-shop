643b00a1887d1acf4bb0d208c1128a6e535e9a35
Joszko <jakub.rezler96@gmail.com>
2024-01-22 22:44:52 +0100

    create summaryPage header

--- src/components/summarypage/SummagePage.jsx
+++ src/components/summarypage/SummagePage.jsx
@@ -1,10 +1,21 @@
 import React from "react";
 import "../../style/css/summaryPage.css";
+import tick from "../../img/tick.png";
 
 const SummaryPage = () => {
 	return (
 		<>
-			<div className="summary-page-wrapper"></div>
+			<div className="summary-page-wrapper">
+				<div className="summary-page-container">
+					<h3 className="summary-page-container__header">
+						Thank you for making a purchase!
+					</h3>
+					<img src={tick} className="summary-page-container__tick-img" />
+					<span className="summary-page-container__delivery-shipped-span">
+						The order will be shipped as soon as we process the payment.
+					</span>
+				</div>
+			</div>
 		</>
 	);
 };
