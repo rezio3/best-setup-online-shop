68bb72918f568ca0f81c517adbefebf51c5e9f9c
Joszko <jakub.rezler96@gmail.com>
2024-01-23 01:11:53 +0100

    create separate component for Order details

---
+++ src/components/summarypage/OrderDetails.jsx
@@ -0,0 +1,72 @@
+import React from "react";
+import ProductFinalList from "./ProductFinalList";
+
+const OrderDetails = (props) => {
+	const { name, surname, phone, email } = props.finalOrder.customer;
+	const { city, postalCode, street, buildingNumber, flatNumber } =
+		props.finalOrder.customer.address;
+
+	const totalProductPrice = props.finalOrder.getTotalProductPrice();
+	const totalInsurancePrice = props.finalOrder.getTotalInsurancePrice();
+	const totalPrice =
+		Number(totalProductPrice) +
+		Number(totalInsurancePrice) +
+		Number(props.finalOrder.customer.deliveryMethod.price);
+	return (
+		<>
+			<div className="final-order-details-container">
+				<div className="final-order-details-box">
+					<div className="final-order-details-box__customer-details-container">
+						<h4 className="final-order-details-box__customer-header">
+							Customer
+						</h4>
+						<span>
+							{name} {surname}
+						</span>
+						<span>{phone}</span>
+						<span>{email}</span>
+						<h4 className="final-order-details-box__customer-header">
+							Delivery address
+						</h4>
+						<span>
+							{city} {postalCode}
+						</span>
+						<span>
+							{street} {buildingNumber}/{flatNumber}
+						</span>
+						<h4 className="final-order-details-box__customer-header">
+							Delivery method
+						</h4>
+						<span>{props.finalOrder.customer.deliveryMethod.method}</span>
+						<h4 className="final-order-details-box__customer-header">
+							Payment method
+						</h4>
+						<span>{props.finalOrder.customer.paymentMethod}</span>
+					</div>
+				</div>
+				<div className="final-order-details-box">
+					<h4 className="final-order-details-box__customer-header">Order</h4>
+					<ul className="summary-list-container">
+						{props.finalOrder.cart.map((e) => {
+							return (
+								<ProductFinalList
+									img={e.img}
+									name={e.name}
+									quantity={e.quantity}
+								/>
+							);
+						})}
+					</ul>
+					<div className="final-order-details-box__total-price-box">
+						<span className="final-order-details-box__price-span">Total:</span>
+						<span className="final-order-details-box__price-span">
+							{totalPrice.toFixed(2)} $
+						</span>
+					</div>
+				</div>
+			</div>
+		</>
+	);
+};
+
+export default OrderDetails;
--- src/components/summarypage/SummaryPage.jsx
+++ src/components/summarypage/SummaryPage.jsx
@@ -5,6 +5,7 @@ import { ProductsOrderContext } from "../../context/OrderContext";
 import ProductFinalList from "./ProductFinalList";
 import FinalCartLowerBox from "../cart/FinalCartLowerBox";
 import GoToHomePageButton from "../GoToHomePageButton";
+import OrderDetails from "./OrderDetails";
 
 const SummaryPage = () => {
 	const [order, setOrder] = useContext(ProductsOrderContext);
@@ -54,14 +55,7 @@ const SummaryPage = () => {
 	// 		);
 	// 	},
 	// });
-	console.log(finalOrder.customer.deliveryMethod.price);
 
-	const totalProductPrice = finalOrder.getTotalProductPrice();
-	const totalInsurancePrice = finalOrder.getTotalInsurancePrice();
-	const totalPrice =
-		Number(totalProductPrice) +
-		Number(totalInsurancePrice) +
-		Number(finalOrder.customer.deliveryMethod.price);
 	return (
 		<>
 			<div className="summary-page-wrapper">
@@ -75,64 +69,7 @@ const SummaryPage = () => {
 					<img src={tick} className="summary-page-container__tick-img" />
 
 					<h3 className="receipt-details-header">Customer's receipt details</h3>
-					<div className="final-order-details-container">
-						<div className="final-order-details-box">
-							<div className="final-order-details-box__customer-details-container">
-								<h4 className="final-order-details-box__customer-header">
-									Customer
-								</h4>
-								<span>
-									{finalOrder.customer.name} {finalOrder.customer.surname}
-								</span>
-								<span>{finalOrder.customer.phone}</span>
-								<span>{finalOrder.customer.email}</span>
-								<h4 className="final-order-details-box__customer-header">
-									Delivery address
-								</h4>
-								<span>
-									{finalOrder.customer.address.city}{" "}
-									{finalOrder.customer.address.postalCode}
-								</span>
-								<span>
-									{finalOrder.customer.address.street}{" "}
-									{finalOrder.customer.address.buildingNumber}/
-									{finalOrder.customer.address.flatNumber}
-								</span>
-								<h4 className="final-order-details-box__customer-header">
-									Delivery method
-								</h4>
-								<span>{finalOrder.customer.deliveryMethod.method}</span>
-								<h4 className="final-order-details-box__customer-header">
-									Payment method
-								</h4>
-								<span>{finalOrder.customer.paymentMethod}</span>
-							</div>
-						</div>
-						<div className="final-order-details-box">
-							<h4 className="final-order-details-box__customer-header">
-								Order
-							</h4>
-							<ul className="summary-list-container">
-								{finalOrder.cart.map((e) => {
-									return (
-										<ProductFinalList
-											img={e.img}
-											name={e.name}
-											quantity={e.quantity}
-										/>
-									);
-								})}
-							</ul>
-							<div className="final-order-details-box__total-price-box">
-								<span className="final-order-details-box__price-span">
-									Total:
-								</span>
-								<span className="final-order-details-box__price-span">
-									{totalPrice.toFixed(2)} $
-								</span>
-							</div>
-						</div>
-					</div>
+					<OrderDetails finalOrder={finalOrder} />
 					<GoToHomePageButton />
 				</div>
 				<FinalCartLowerBox />
