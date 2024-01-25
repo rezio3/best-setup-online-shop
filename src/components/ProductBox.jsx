431ddf12b6439b728df0577aba13b0c691801082
Joszko <jakub.rezler96@gmail.com>
2024-01-25 12:19:51 +0100

    update ProductPage.jsx component

--- src/components/ProductBox.jsx
+++ src/components/ProductBox.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useRef, useState } from "react";
+import React, { useContext, useRef, useState } from "react";
 import { gsap } from "gsap";
 import "../style/css/buttonsAnim.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
--- src/components/productPage/ProductPage.jsx
+++ src/components/productPage/ProductPage.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "../../style/css/productPage.css";
 
 const ProductPage = () => {
 	return <></>;
