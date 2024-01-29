ba9bd2fc560dedf8dc9d3d50be980d9a504e641a
Joszko <jakub.rezler96@gmail.com>
2024-01-29 14:43:30 +0100

    add NavLink to RecommendedProduct.jsx component

--- src/components/cart/RecommendedProduct.jsx
+++ src/components/cart/RecommendedProduct.jsx
@@ -1,26 +1,29 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
+import { NavLink } from "react-router-dom";
 
 const RecommendedProduct = (props) => {
 	return (
 		<>
-			<div className="recommended-product-box">
-				<div className="recommended-product-box__product-image-container">
-					<img
-						src={props.product.img}
-						alt="recommended-product"
-						className="recommended-product-box__product-image"
-					/>
+			<NavLink to={`/product/hotDeals/${props.product.productId}`}>
+				<div className="recommended-product-box">
+					<div className="recommended-product-box__product-image-container">
+						<img
+							src={props.product.img}
+							alt="recommended-product"
+							className="recommended-product-box__product-image"
+						/>
+					</div>
+					<span className="recommended-product-box__product-name">
+						{props.product.name}
+					</span>
+					<span className="recommended-product-box__read-more">
+						READ MORE{" "}
+						<FontAwesomeIcon icon={faArrowRight} className="highlight" />
+					</span>
 				</div>
-				<span className="recommended-product-box__product-name">
-					{props.product.name}
-				</span>
-				<span className="recommended-product-box__read-more">
-					READ MORE{" "}
-					<FontAwesomeIcon icon={faArrowRight} className="highlight" />
-				</span>
-			</div>
+			</NavLink>
 		</>
 	);
 };
