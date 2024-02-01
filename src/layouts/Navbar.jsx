ef47d36b59a284154a256726518c473849ac3306
Joszko <jakub.rezler96@gmail.com>
2024-02-01 01:17:57 +0100

    delete unneccessary code in Navbar.jsx

--- src/layouts/Navbar.jsx
+++ src/layouts/Navbar.jsx
@@ -8,7 +8,6 @@ const Navbar = () => {
 		<>
 			<Media query="(min-width: 992px)">
 				{(matches) => {
-					console.log(matches);
 					return matches ? <NavbarDesktop /> : <NavbarMobile />;
 				}}
 			</Media>
